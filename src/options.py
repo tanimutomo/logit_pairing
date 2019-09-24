4aa0c76bb93410d938bca7d1443ff8ff930ed48e
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2019-09-24 23:25:57 +0900

    Add device settings for data parallel

--- src/options.py
+++ src/options.py
@@ -1,11 +1,12 @@
 import argparse
 import random
+import torch
 
 class Parser():
     default_options = dict(
         mnist=dict(batch_size=200,
                    num_epochs=500,
-                   model='lenet',
+                   arch='lenet',
                    eps=76.5,
                    eps_iter=2.55,
                    num_steps=40,
@@ -15,7 +16,7 @@ class Parser():
 
         cifar10=dict(batch_size=128,
                      num_epochs=100,
-                     model='resnet',
+                     arch='resnet',
                      eps=16.0,
                      eps_iter=2.0,
                      num_steps=10, clip_min=0.0, clip_max=1.0,
@@ -27,8 +28,7 @@ class Parser():
             formatter_class=argparse.ArgumentDefaultsHelpFormatter
         )
         # device
-        parser.add_argument('--gpu_id', type=int, default=0, help='id of gpu')
-        parser.add_argument('--cuda', action='store_true', help='use cuda')
+        parser.add_argument('--gpu_ids', type=int, nargs='*', default=[], help='id of gpu')
 
         # data
         parser.add_argument('--data_root', type=str, default='~/data', help='root of dataset')
@@ -39,8 +39,8 @@ class Parser():
         parser.add_argument('--noise_std', type=float, help='std of gaussian')
         parser.add_argument('--val_samples', type=int, default=1000, help='number of samples for validation')
 
-        # model
-        parser.add_argument('--model', type=str, choices=['lenet', 'resnet'], help='model name')
+        # architecture
+        parser.add_argument('--arch', type=str, choices=['lenet', 'resnet'], help='architecture name')
         parser.add_argument('--weight_init', type=str, default='he', help='method of weight initialization')
 
         # loss
@@ -53,7 +53,7 @@ class Parser():
         parser.add_argument('--wd', type=float, default=0.0, help='weight decay')
 
         # attack
-        parser.add_argument('--attacker', type=str, default='pgd', help='name of adversarial attack method')
+        parser.add_argument('--attack', type=str, default='pgd', help='name of adversarial attack method')
         parser.add_argument('--eps', type=float, help='epsilon for lp-norm attack')
         parser.add_argument('--eps_iter', type=float, help='epsilon for each attack step')
         parser.add_argument('--num_steps', type=int, help='number of steps for attack')
@@ -61,7 +61,7 @@ class Parser():
         parser.add_argument('--clip_max', type=float, help='miximum value for cliping AEs')
 
         # optimization
-        parser.add_argument('--optimizer', type=str, default='Adam', help='name of optimization method')
+        parser.add_argument('--optim', type=str, default='Adam', help='name of optimization method')
         parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
         parser.add_argument('--scheduler_step', type=int, default=0, help='step for lr-scheduler')
         parser.add_argument('--scheduler_gamma', type=float, default=0.1, help='gamma for lr-scheduler')
@@ -99,5 +99,21 @@ class Parser():
 
         self.opt.add_names.append(str(random.randint(100, 999)))
         self.opt.exp_name = '_'.join(self.opt.add_names)
+
+        # set device
+        if torch.cuda.is_available():
+            if not self.opt.gpu_ids:
+                self.opt.device = torch.device('cuda:0')
+                self.opt.gpu_ids = [i for i in range(torch.cuda.device_count())]
+            elif len(self.opt.gpu_ids) > 1:
+                self.opt.device = torch.device('cuda:{}'.format(self.opt.gpu_ids[0]))
+            else:
+                self.opt.device = torch.device('cuda:{}'.format(self.opt.gpu_ids[0]))
+                self.opt.gpu_ids = []
+            self.opt.cuda = True
+        else:
+            self.opt.device = torch.device('cpu')
+            self.opt.gpu_ids = []
+            self.opt.cuda = False
 
         return self.opt
