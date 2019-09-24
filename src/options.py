b8d06a10720b0080035d8c72edb4350057589129
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2019-09-25 00:49:38 +0900

    Update for containing test options

--- src/options.py
+++ src/options.py
@@ -23,7 +23,9 @@ class Parser():
                      noise_std=0.06)
         )
 
-    def __init__(self):
+    def __init__(self, train=True):
+        self.train = train
+
         parser = argparse.ArgumentParser(
             formatter_class=argparse.ArgumentDefaultsHelpFormatter
         )
@@ -35,22 +37,10 @@ class Parser():
         parser.add_argument('--dataset', type=str, required=True, choices=['mnist', 'cifar10'],
                             help='dataset name')
         parser.add_argument('--batch_size', type=int, help='batch size')
-        parser.add_argument('--noise', action='store_true', help='gaussian noise')
-        parser.add_argument('--noise_std', type=float, help='std of gaussian')
-        parser.add_argument('--val_samples', type=int, default=1000, help='number of samples for validation')
+        parser.add_argument('--num_val_samples', type=int, default=1000, help='number of samples for validation')
 
         # architecture
         parser.add_argument('--arch', type=str, choices=['lenet', 'resnet'], help='architecture name')
-        parser.add_argument('--weight_init', type=str, default='he', help='method of weight initialization')
-
-        # loss
-        parser.add_argument('--ct', type=float, default=0.0, help='coef for clean example training')
-        parser.add_argument('--at', type=float, default=0.0, help='coef of adversarial example training')
-        parser.add_argument('--alp', type=float, default=0.0, help='coef of adversarial logit pairing')
-        parser.add_argument('--clp', type=float, default=0.0, help='coef of clean logit pairing')
-        parser.add_argument('--lsq', type=float, default=0.0, help='coef of logit squeezing')
-        parser.add_argument('--lsq_grad', action='store_true', help='use gradual coef for logit squeezing')
-        parser.add_argument('--wd', type=float, default=0.0, help='weight decay')
 
         # attack
         parser.add_argument('--attack', type=str, default='pgd', help='name of adversarial attack method')
@@ -60,45 +50,67 @@ class Parser():
         parser.add_argument('--clip_min', type=float, help='minimum value for cliping AEs')
         parser.add_argument('--clip_max', type=float, help='miximum value for cliping AEs')
 
-        # optimization
-        parser.add_argument('--optim', type=str, default='Adam', help='name of optimization method')
-        parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
-        parser.add_argument('--scheduler_step', type=int, default=0, help='step for lr-scheduler')
-        parser.add_argument('--scheduler_gamma', type=float, default=0.1, help='gamma for lr-scheduler')
-        parser.add_argument('--num_epochs', type=int, help='number of epochs')
+        if train:
+            # loss
+            parser.add_argument('--ct', type=float, default=0.0, help='coef for clean example training')
+            parser.add_argument('--at', type=float, default=0.0, help='coef of adversarial example training')
+            parser.add_argument('--alp', type=float, default=0.0, help='coef of adversarial logit pairing')
+            parser.add_argument('--clp', type=float, default=0.0, help='coef of clean logit pairing')
+            parser.add_argument('--lsq', type=float, default=0.0, help='coef of logit squeezing')
+            parser.add_argument('--lsq_grad', action='store_true', help='use gradual coef for logit squeezing')
+            parser.add_argument('--wd', type=float, default=0.0, help='weight decay')
 
-        # others
-        parser.add_argument('--comet', action='store_true', help='use comet for training log')
-        parser.add_argument('--print_freq', type=int, default=10, help='frequency of printing logs')
-        parser.add_argument('--adv_val_freq', type=int, default=10, help='frequency of adversarial validation')
-        parser.add_argument('--add_names', type=str, nargs='*', default=[], help='additional experiment name')
-        parser.add_argument('--add_tags', type=str, nargs='*', default=[], help='additinal tags for comet')
+            # noise
+            parser.add_argument('--noise', action='store_true', help='gaussian noise')
+            parser.add_argument('--noise_std', type=float, help='std of gaussian')
 
-        # debug
-        parser.add_argument('--report_itr_loss', type=str, nargs='*', default=[], 
-                            choices=['ct', 'at', 'alp', 'clp', 'lsq'],
-                            help='report loss each iteration in the training phase')
+            # optimization
+            parser.add_argument('--optim', type=str, default='Adam', help='name of optimization method')
+            parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
+            parser.add_argument('--scheduler_step', type=int, default=0, help='step for lr-scheduler')
+            parser.add_argument('--scheduler_gamma', type=float, default=0.1, help='gamma for lr-scheduler')
+            parser.add_argument('--num_epochs', type=int, help='number of epochs')
+
+            # weight initialization
+            parser.add_argument('--weight_init', type=str, default='he', help='method of weight initialization')
+
+            # others
+            parser.add_argument('--comet', action='store_true', help='use comet for training log')
+            parser.add_argument('--print_freq', type=int, default=10, help='frequency of printing logs')
+            parser.add_argument('--adv_val_freq', type=int, default=10, help='frequency of adversarial validation')
+            parser.add_argument('--add_names', type=str, nargs='*', default=[], help='additional experiment name')
+            parser.add_argument('--add_tags', type=str, nargs='*', default=[], help='additinal tags for comet')
+
+            # debug
+            parser.add_argument('--report_itr_loss', type=str, nargs='*', default=[], 
+                                choices=['ct', 'at', 'alp', 'clp', 'lsq'],
+                                help='report loss each iteration in the training phase')
+
+        else:
+            # path to trained weight
+            parser.add_argument('--weight_path', type=str, required=True, help='path to trained weight')
 
         self.opt = parser.parse_args()
 
     def get(self):
         # Set None options in default_options
         for name, value in self.default_options[self.opt.dataset].items():
-            if not getattr(self.opt, name):
+            if hasattr(self.opt, name) and not getattr(self.opt, name):
                 setattr(self.opt, name, value)
 
 
         # Add tags and set experiment name
-        base_tags = ['{}e'.format(self.opt.num_epochs),
-                     self.opt.dataset]
-        for name in ['ct', 'at', 'alp', 'clp', 'lsq']:
-            if getattr(self.opt, name):
-                base_tags.append(name)
-                self.opt.add_names.append(name)
-        self.opt.add_tags.extend(base_tags)
+        if self.train:
+            base_tags = ['{}e'.format(self.opt.num_epochs),
+                         self.opt.dataset]
+            for name in ['ct', 'at', 'alp', 'clp', 'lsq']:
+                if getattr(self.opt, name):
+                    base_tags.append(name)
+                    self.opt.add_names.append(name)
+            self.opt.add_tags.extend(base_tags)
 
-        self.opt.add_names.append(str(random.randint(100, 999)))
-        self.opt.exp_name = '_'.join(self.opt.add_names)
+            self.opt.add_names.append(str(random.randint(100, 999)))
+            self.opt.exp_name = '_'.join(self.opt.add_names)
 
         # set device
         if torch.cuda.is_available():
